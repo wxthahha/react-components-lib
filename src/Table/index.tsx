24422b8a3283b865f1e91aca3941ad653de5bcc5
wangxiaoting <wangxiaoting@4paradigm.com>
2023-11-17 18:28:32 +0800

    init

---
+++ src/Table/index.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: wangxiaoting wangxiaoting@4paradigm.com
+ * @Date: 2023-11-17 11:17:22
+ * @LastEditors: wangxiaoting wangxiaoting@4paradigm.com
+ * @LastEditTime: 2023-11-17 11:26:14
+ * @Description: demo-card
+ */
+import React from "react";
+
+function Table() {
+  return <div>Table</div>;
+}
+
+export default Table;
