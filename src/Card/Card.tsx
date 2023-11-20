bc63c696fe85fdb25232441122a2ec0ab922996e
wangxiaoting <wangxiaoting@4paradigm.com>
2023-11-20 17:27:51 +0800

    feat:docz

---
+++ src/Card/Card.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: wangxiaoting wangxiaoting@4paradigm.com
+ * @Date: 2023-11-17 11:17:18
+ * @LastEditors: wangxiaoting wangxiaoting@4paradigm.com
+ * @LastEditTime: 2023-11-20 17:19:57
+ * @Description: demo-card
+ */
+import React from "react";
+
+// const Card: React.FC<{
+//   size?: "large" | "middle" | "small";
+// }> = (props) => {
+//   const { size } = props;
+//   return <div>Card - {size}</div>;
+// };
+function Card() {
+  return <div>Card</div>;
+}
+
+export default Card;
