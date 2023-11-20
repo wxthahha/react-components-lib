6bbfc676781ae95d62d3807229a2eba909325165
wangxiaoting <wangxiaoting@4paradigm.com>
2023-11-20 17:28:46 +0800

    feat:docz

--- .docz/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---src-card-card-mdx": () => import("./../../../../src/Card/Card.mdx" /* webpackChunkName: "component---src-card-card-mdx" */),
-  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-table-table-mdx": () => import("./../../../../src/Table/Table.mdx" /* webpackChunkName: "component---src-table-table-mdx" */)
-}
-
