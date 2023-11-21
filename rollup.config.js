8cd76cd0b1ca6116efa53504d6633c7a092b1a04
wangxiaoting <wangxiaoting@4paradigm.com>
2023-11-21 14:31:14 +0800

    feat:docz

--- rollup.config.js
+++ rollup.config.js
@@ -2,33 +2,55 @@
  * @Author: wangxiaoting wangxiaoting@4paradigm.com
  * @Date: 2023-11-17 15:17:57
  * @LastEditors: wangxiaoting wangxiaoting@4paradigm.com
- * @LastEditTime: 2023-11-17 18:24:13
+ * @LastEditTime: 2023-11-21 11:28:48
  * @Description: rollup配置文件
  */
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel, { getBabelOutputPlugin } from "@rollup/plugin-babel";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import jsx from "acorn-jsx";
+import eslint from "@rollup/plugin-eslint";
+
+const overrides = {
+  compilerOptions: {
+    noUnusedParameters: true,
+    noUnusedLocals: true,
+    strictNullChecks: true,
+    moduleResolution: "node",
+    declaration: true, //抽离声明代码 *.d.js
+    allowSyntheticDefaultImports: true,
+  },
+  useTsconfigDeclarationDir: true,
+};
 
 module.exports = {
   input: "src/index.ts",
   acornInjectPlugins: [jsx()],
   plugins: [
-    resolve(),
+    resolve({
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".less"], //允许我们加载第三方模块
+    }),
     commonjs(),
     postcss({
       extract: true,
       modules: true,
+      extensions: [".less", ".css"],
+      namedExports: true,
+      minimize: true,
     }),
     typescript({
       jsx: "preserve",
+      tsconfigOverride: overrides,
     }),
     babel({
       presets: ["@babel/preset-react"],
       babelHelpers: "bundled",
-      extensions: [".ts", ".tsx"],
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    }),
+    eslint({
+      exclude: ["node_modules"],
     }),
   ],
   output: [
@@ -44,11 +66,12 @@ module.exports = {
     {
       file: "libs/umd/index.umd.js",
       format: "umd",
-      name: "BizBaseUI"
+      name: "BizBaseUI",
     },
     {
       dir: "libs/es",
       format: "es",
-    }
+    },
   ],
+  external: ["react", "react-dom"],
 };
--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -2,7 +2,7 @@
  * @Author: wangxiaoting wangxiaoting@4paradigm.com
  * @Date: 2023-11-17 11:17:18
  * @LastEditors: wangxiaoting wangxiaoting@4paradigm.com
- * @LastEditTime: 2023-11-20 17:19:57
+ * @LastEditTime: 2023-11-21 14:30:10
  * @Description: demo-card
  */
 import React from "react";
